b2fbccd41461dc1e796cf1d548967abe129d9b56
MikaelRex <76702390+MikaelRex@users.noreply.github.com>
2021-02-25 17:35:19 -0600

    15/02/21
    MJP

---
+++ Mitre_JSONv3.py
@@ -0,0 +1,18 @@
+import json
+import os.path
+import sys
+
+#print("########################################################")
+#print("##########   INGRESA NOMBRE DE ARTEFACTO    ############")
+#print("########################################################")
+
+ArtifactName= sys.argv[1]
+
+if (ArtifactName!="Miguel"):
+    
+    print("es: "+ArtifactName)
+else:
+    print("test ")
+        
+
+
